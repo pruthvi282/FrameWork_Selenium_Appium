87af618a118a555984b6b7e2b2f44551d0760fe4
Admin <Admin@DESKTOP-ODJGIG9>
2021-03-12 16:17:44 +0530

    s

---
+++ testng/src/project1/Demo.java
@@ -0,0 +1,13 @@
+package project1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void test1(){
+		
+		Reporter.log("hello",true);
+	}
+	
+}
