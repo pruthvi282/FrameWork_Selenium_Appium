d3cc113d7abcc3cb96438a1b707119f15ee316ee
Admin <Admin@DESKTOP-ODJGIG9>
2021-03-15 13:34:39 +0530

    asasa

---
+++ MavenProject/src/test/java/vtiger/Test.java
@@ -0,0 +1,18 @@
+package vtiger;
+
+import org.testng.annotations.BeforeClass;
+
+public class Test {
+@org.testng.annotations.Test
+public void launch()
+{
+	
+	
+	System.out.println(System.getProperty("url"));
+	System.out.println(System.getProperty("username"));
+	System.out.println(System.getProperty("password"));
+	
+	
+}
+	
+}
